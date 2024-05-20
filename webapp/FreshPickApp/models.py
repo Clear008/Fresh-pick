eeaa40c5e5a1ec84604d0b1f7843d79a08b79666
Clear008 <wissal-exo@hotmail.com>
2024-05-20 15:46:27 +0100

    update models

--- webapp/FreshPickApp/models.py
+++ webapp/FreshPickApp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    shipping_address = models.TextField()
+
+    def __str__(self):
+        return self.username
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity_available = models.IntegerField()
+    image = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in {self.cart}"
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in order {self.order.id}"
